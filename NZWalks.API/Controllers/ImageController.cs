5a99100f319309133e3923d8a929b113864ff994
Enata <emmanueltag123@gmail.com>
2024-01-30 17:01:41 +0000

    fixed a bug in the image controller

--- NZWalks.API/Controllers/ImageController.cs
+++ NZWalks.API/Controllers/ImageController.cs
@@ -37,7 +37,7 @@ public class ImageController : ControllerBase
             };
 
 
-            var result =  _repository.Upload(imageDomain);
+            var result =  await _repository.Upload(imageDomain);
         
         
             return Ok(result);
